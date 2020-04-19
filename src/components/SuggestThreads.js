7c354a06179c8371972e71ebd96f68310b96297f
boranorben <zhotkp@gmail.com>
2020-04-20 03:05:36 +0700

    re css added on suggest threads

--- src/components/SuggestThreads.js
+++ src/components/SuggestThreads.js
@@ -1,231 +1,196 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router';
+import { withRouter, Link } from 'react-router-dom';
 import axios from 'axios';
 import qs from 'qs';
+import * as ROUTES from '../constants/routes';
 
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import { Carousel, Col, Row, Tag, Button, Menu, Icon, Dropdown } from 'antd';
+import 'antd/dist/antd.css';
 import "../css/suggest.css";
+import { Carousel, Col, Row, Tag, Button, Menu, Icon, Dropdown } from 'antd';
 
 import SuggestDuration from "../components/SuggestDuration";
 import SuggestMonth from "../components/SuggestMonth";
 import SuggestTheme from "../components/Theme";
 
 const { SubMenu } = Menu;
-const backend_url = process.env.REACT_APP_BACKEND_URL || 'localhost:30010'
+const backend_url = process.env.REACT_APP_BACKEND_URL || 'localhost:30010';
 
 class SuggestThreads extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            threads: [],
-            threadSuggest: [],
-            threadProperties: [],
-            list: [],
-            value: 1,
-            radio: 1,
-            fullData: [],
-            query: {},
-            withThread: 2
-        };
-    }
-
-    onBlur() {
-        console.log('blur');
-    }
-
-    onFocus() {
-        console.log('focus');
-    }
-
-    onSearch(val) {
-        console.log('search:', val);
-    }
-
-    // inCountry = (e) => {
-    //     const query = this.state.query;
-    //     query.within_th = 1;
-    //     console.log('in thai' + query.within_th);
-    //     this.setState({ query: query });
-    //     this.getInformation(query);
-
-    // }
-
-    // outCountry = (e) => {
-    //     const query = this.state.query;
-    //     query.within_th = 0;
-    //     console.log('out thai' + query.within_th);
-    //     this.setState({ query: query });
-    //     this.getInformation(query);
-    // };
-
-    handleCountry = (withType, withThread) => {
-        const query = this.state.query;
-        query.within_th = withType
-        console.log("check query.within_th: " + query.within_th)
-        console.log("check withType: " + withType)
-        this.setState({
-            query: query,
-            withThread
-        });
-        this.getInformation(query);
-    }
-
-    async getInformation(query) {
-        let response = null;
-        const q = qs.stringify(query, { addQueryPrefix: true, arrayFormat: 'comma' })
-        this.props.history.push(`/${q}`);
-        try {
-            response = await axios.get(`http://${backend_url}/api/home/suggestThreads${q}`)
-        } catch (error) {
-            console.log(error);
-        }
-        if (response) {
-            // Map data after get response
-            this.mapData(response);
-        }
-    }
-
-    mapData(response) {
-        const threadProperties = response.data.map(item => {
-            return {
-                ...item,
-                country: item.countries.map(c => c.nameEnglish + " "),
-            };
-        });
-        this.setState({
-            threadProperties: threadProperties,
-        });
-    }
-
-    getQueryParams(value) {
-        return qs.parse(value, { ignoreQueryPrefix: true })
-    }
-
-    componentDidMount() {
-        const q = this.getQueryParams(this.props.location.search);
-        this.setState({
-            query: q
-        })
-        this.getInformation(q);
-    }
-
-    CreateSuggest(startIndex) {
-        if (this.state.threadProperties < 1) {
-            return 'Loading'
-        }
-
-        const list = [
-            this.state.threadProperties[startIndex],
-            this.state.threadProperties[startIndex + 1],
-            this.state.threadProperties[startIndex + 2]
-        ]
-
-        const menu = (
-            <Menu>
-                <SubMenu title="Add to My Triplist">
-                    <Menu.Item>New Triplist</Menu.Item>
-                    <Menu.Item>Japan Trip</Menu.Item>
-                </SubMenu>
-                <Menu.Item>Save to My Favorite</Menu.Item>
-                <Menu.Item>Share</Menu.Item>
-            </Menu>
-        );
-
-        return list.map(d => {
-            return (
-                <Col>
-                    <Col span={3}>
-                        <img
-                            style={{ width: 100, height: 100, margin: "15px" }}
-                            alt="example"
-                            src={d.thumbnail}
-                        />
-                    </Col>
-                    <Col span={5} style={{ lineHeight: 'normal', marginTop: '20px' }}>
-                        <Row>
-                            <a href={d.link} target="_blank" rel="noopener noreferrer" style={{ color: "#181741" }}>
-                                {d.title}
-                            </a>
-                        </Row>
-                        <Row style={{ marginTop: "3%" }}>
-                            <Tag color="rgba(130, 142, 180, 0.5)">{d.country}</Tag>
-
-                            <Icon type="heart"
-                                theme={this.state.heartFavorites}
-                                onClick={this.onHeartFavoriteClick}
-                                style={{ width: `5%`, margin: `auto 0 auto 2%`, fontSize: '23px', color: '#10828C' }} />
-                            {/* <Icon type="more" style={{ width: `5%`, margin: 'auto', fontSize: '23px' }} /> */}
-                            <Dropdown overlay={menu}>
-                                <a className="ant-dropdown-link" href="#" style={{ marginLeft: "5%" }}>
-                                    <Icon type="more" style={{ color: "#10828C", width: `5%`, margin: 'auto', fontSize: '23px' }} />
-                                </a>
-                            </Dropdown>
-
-                        </Row>
-                    </Col>
-                </Col>
-            );
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      threadProperties: [],
+      query: {},
+      withThread: 2
     };
+  }
 
-    render() {
-        return (
-            <div>
-                <Row style={{ marginLeft: "15%", marginRight: "15%" }}>
-                    <Col span={12}>
-                        <Button
-                            id="with"
-                            size="default"
-                            style={{ width: "100%", borderRadius: 0, paddingLeft: "50px", paddingRight: "50px", marginTop: "25px" }}
-                            onClick={() => this.handleCountry('1', 1)}
-                            className={`type-btn ${this.state.withThread === 1 ? 'active' : ''}`}
-                            value={this.state.query.within_th}>
-                            Within Thailand
-                            </Button>
-                    </Col>
-                    <Col span={12}>
-                        <Button
-                            id="with"
-                            size="default"
-                            style={{ width: "100%", borderRadius: 0, paddingLeft: "50px", paddingRight: "50px", marginTop: "25px" }}
-                            onClick={() => this.handleCountry('0', 2)}
-                            className={`type-btn ${this.state.withThread === 2 ? 'active' : ''}`}
-                            value={this.state.query.within_th}>
-                            International Countries
-                    </Button>
-                    </Col>
-                </Row>
+  getQueryParams = (value) => {
+    return qs.parse(value, { ignoreQueryPrefix: true })
+  }
 
-                <div id="first-pop-suggest-thread">
-                    <Icon
-                        type="fire"
-                        theme="filled"
-                        style={{ marginRight: "19px" }}
-                    />Popular Suggestion Threads</div>
-                <Carousel autoplay style={{ marginLeft: "50px", marginRight: "40px", marginBottom: "20px" }}>
+  componentDidMount() {
+    const query = this.getQueryParams(this.props.location.search);
+    this.setState({
+      query: query
+    })
 
-                    <div>
-                        {this.CreateSuggest(0)}
-                    </div>
-                    <div>
-                        {this.CreateSuggest(3)}
-                    </div>
-                    <div>
-                        {this.CreateSuggest(6)}
-                    </div>
-                    <div>
-                        {this.CreateSuggest(9)}
-                    </div>
+    this.getThreads(query)
+  }
 
-                </Carousel>
-                <SuggestDuration within={this.state.query.within_th} />
-                <SuggestMonth within={this.state.query.within_th} />
-                <SuggestTheme within={this.state.query.within_th} />
-            </div>
+  onBoundClinked = (type, thread) => {
+    const query = this.state.query;
+    query.within_th = type;
+    console.log(this.state.query.within_th)
+    this.setState({
+      query: query,
+      withThread: thread
+    });
 
-        )
+    this.getThreads(query)
+  }
+
+  getThreads = async (query) => {
+    let response = null;
+    const q = qs.stringify(query, { addQueryPrefix: true, arrayFormat: 'comma' });
+    this.props.history.push(`/${q}`);
+
+    try {
+      response = await axios.get(`http://${backend_url}/api/home/suggestThreads${q}`)
+    } catch (error) {
+      console.log(error);
     }
+
+    if (response) {
+      this.mapData(response);
+    }
+  }
+
+  mapData(response) {
+    const threadProperties = response.data.map(item => {
+      return {
+        ...item,
+        country: item.countries.map(c => c.nameEnglish + " "),
+      }
+    })
+
+    this.setState({
+      threadProperties: threadProperties,
+    })
+  }
+
+  createSuggestion = (startIndex) => {
+    if (this.state.threadProperties < 1) {
+      return 'Loading'
+    }
+
+    const threadList = [
+      this.state.threadProperties[startIndex],
+      this.state.threadProperties[startIndex + 1],
+      this.state.threadProperties[startIndex + 2]
+    ]
+
+    const menu = (
+      <Menu>
+        <SubMenu title='Add to My Triplist'>
+          <Menu.Item>New Triplist</Menu.Item>
+          {/* <Menu.Item>Japan Trip</Menu.Item> */}
+        </SubMenu>
+        <Menu.Item>Save to My Favorite</Menu.Item>
+        <Menu.Item>Share</Menu.Item>
+      </Menu>
+    )
+
+    return threadList.map(thread => {
+      return (
+        <Col span={8} className='thread-card'>
+          <Col span={12}>
+            <img src={thread.thumbnail} />
+          </Col>
+          <Col span={12} className='thread-info'>
+            <Row className='thread-title'>
+              <Col>
+                <a href={`https://pantip.com/topic/${thread.topic_id}`}>
+                  {thread.title}
+                </a>
+              </Col>
+            </Row>
+            <Row className='thread-option'>
+              <Col span={12} id='tag'>
+                <Tag>{thread.country[0]}</Tag>
+              </Col>
+              <Col span={12} id='icon'>
+                <Icon
+                  type='heart'
+                  theme={this.state.heartFavorites}
+                  onClick={this.onHeartFavoriteClick}
+                />
+                <Dropdown overlay={menu}>
+                  <Icon type='more' />
+                </Dropdown>
+              </Col>
+            </Row>
+          </Col>
+        </Col>
+      )
+    })
+  }
+
+  getCarousel = () => {
+    const carouselIndex = [0, 3, 6, 9]
+    return carouselIndex.map(index => {
+      return (
+        <div>{this.createSuggestion(index)}</div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <Row id='suggest' className='bound-btn'>
+          <Button
+            size='large'
+            onClick={() => { this.onBoundClinked('1', 1) }}
+            value={this.state.query.within_th}
+            className={`type-btn ${this.state.withThread === 1 ? 'active' : ''}`}
+            id='selectBtn'
+          >
+            Within Thailand
+          </Button>
+          <Button
+            size='large'
+            onClick={() => { this.onBoundClinked('0', 2) }}
+            value={this.state.query.within_th}
+            className={`type-btn ${this.state.withThread === 2 ? 'active' : ''}`}
+            id='selectBtn'
+          >
+            International Countries
+          </Button>
+        </Row>
+        <Row className='suggestion-threads'>
+          <Col span={12} className='suggest-title'>
+            <Icon
+              type='fire'
+              theme='filled'
+            />
+            <p>&nbsp;&nbsp; Popular <span>Suggestion Threads</span></p>
+          </Col>
+          <Col span={12} className='see-more'>
+            <Link to={ROUTES.FORUMS + '?type=suggest'}>See more</Link>
+          </Col>
+          <Col span={24}>
+            <Carousel autoplay>
+              {this.getCarousel()}
+            </Carousel>
+          </Col>
+        </Row>
+        <SuggestDuration within={this.state.query.within_th} />
+        <SuggestMonth within={this.state.query.within_th} />
+        <SuggestTheme within={this.state.query.within_th} />
+      </div>
+    )
+  }
 }
 
 export default withRouter(SuggestThreads);
